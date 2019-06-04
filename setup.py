4012bd77a3071ba5d87a798cc9619032ae266f58
Edmund Pfeil <edmundpf@buffalo.edu>
2019-06-04 14:15:09 -0400

    Removed obsolete requirements. PyPi v0.1.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="file-tools",
-	version="0.1.2",
+	version="0.1.3",
 	author="Edmund Pfeil",
 	author_email="edmundpf@buffalo.edu",
 	description="File and JSON File Methods.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/edmundpf/print_tools",
-	install_requires=['colorful'],
+	install_requires=[],
 	packages=setuptools.find_packages(),
 	classifiers=[
 		"Programming Language :: Python :: 3",
