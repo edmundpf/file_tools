e6d909a75f6c8af756f754b51bc582f44193215a
Edmund Pfeil <edmundpf@buffalo.edu>
2019-06-04 12:21:33 -0400

    Fixed caller file path. PyPi v0.0.5

--- file_tools/file.py
+++ file_tools/file.py
@@ -2,13 +2,13 @@ from file_tools.utils.functions import get_file
 
 #: Get File String
 
-def get_file_string(file, relative=False, path=''):
+def get_file_string(file, relative=False):
 	with open(get_file(file, relative), 'r') as f:
 		return f.read()
 
 #: Get File Lines
 
-def get_file_lines(file, newline='\n', relative=False, path=''):
+def get_file_lines(file, newline='\n', relative=False):
 	with open(get_file(file, relative), 'r') as f:
 		return [l.rstrip(newline) for l in f]
 
--- file_tools/json_file.py
+++ file_tools/json_file.py
@@ -3,7 +3,7 @@ from file_tools.utils.functions import get_file
 
 #: Import JSON
 
-def import_json(file, relative=False, path=''):
+def import_json(file, relative=False):
 	with open(get_file(file, relative), 'r') as f:
 		try:
 			return json.load(f)
@@ -12,7 +12,7 @@ def import_json(file, relative=False, path=''):
 
 #: Export JSON
 
-def export_json(data, file, indent=2, relative=False, path=''):
+def export_json(data, file, indent=2, relative=False):
 	with open(get_file(file, relative), 'w') as f:
 		json.dump(data, f, indent=indent)
 
