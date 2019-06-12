3982321c25eb691b9dc10a9e04ab87522ea5dcdb
Edmund Pfeil <edmundpf@buffalo.edu>
2019-06-12 10:34:35 -0400

    Added file writing functions.

--- file_tools/file.py
+++ file_tools/file.py
@@ -12,4 +12,32 @@ def get_file_lines(file, path='', newline='\n'):
 	with open(get_file(file, path=path), 'r') as f:
 		return [l.rstrip(newline) for l in f]
 
+#: Write File String
+
+def write_file_string(file, path='', text=''):
+	with open(get_file(file, path=path), 'w') as f:
+		f.write(text)
+		return True
+
+#: Append File String
+
+def append_file_string(file, path='', text=''):
+	with open(get_file(file, path=path), 'a') as f:
+		f.write(text)
+		return True
+
+#: Write File Lines
+
+def write_file_lines(file, path='', lines=[]):
+	with open(get_file(file, path=path), 'a') as f:
+		f.writelines(lines)
+		return True
+
+#: Append File Lines
+
+def append_file_lines(file, path='', lines=[]):
+	with open(get_file(file, path=path), 'a') as f:
+		f.writelines(lines)
+		return True
+
 #::: END PROGRAM :::
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="file-tools",
-	version="0.1.4",
+	version="0.1.5",
 	author="Edmund Pfeil",
 	author_email="edmundpf@buffalo.edu",
 	description="File and JSON File Methods.",
